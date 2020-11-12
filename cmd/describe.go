309499c3889fc8fd24b3509c6bc7260f4ea2e951
Thiago Henrique de Paulo Lima <thiago.lima@semantix.com.br>
2020-11-11 22:31:45 -0300

    feat: processing commands using func ExecuteCommand

--- cmd/describe.go
+++ cmd/describe.go
@@ -1,23 +1,17 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 	"github.com/thiagohdeplima/opengalaxy-cli/cmd/helpers"
 )
 
-// describeExec print a list of requested resourcces
-func describeExec(cmd *cobra.Command, args []string) {
-	fmt.Printf("TODO: Implement command %s\n", cmd.Name())
-}
-
 // DescribeCmd command to list resources
 var DescribeCmd = &cobra.Command{
-	Use:                   "describe [ARGUMENT]",
-	Short:                 "describe a resource",
-	Args:                  helpers.ValidateArgs,
-	ValidArgs:             []string{"galaxies"},
-	Run:                   describeExec,
+	Use:       "describe [ARGUMENT]",
+	Short:     "describe a resource",
+	Args:      helpers.ValidateArgs,
+	ValidArgs: []string{"galaxies"},
+	Run:       ExecuteCommand,
+
 	DisableFlagsInUseLine: true,
 }
--- cmd/list.go
+++ cmd/list.go
@@ -1,23 +1,17 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 	"github.com/thiagohdeplima/opengalaxy-cli/cmd/helpers"
 )
 
-// RunList print a list of requested resources
-func RunList(cmd *cobra.Command, args []string) {
-	fmt.Printf("TODO: Implement command %s\n", cmd.Name())
-}
-
 // ListCmd command to list resources
 var ListCmd = &cobra.Command{
-	Use:                   "list [ARGUMENT]",
-	Short:                 "list resources",
-	Args:                  helpers.ValidateArgs,
-	ValidArgs:             []string{"galaxies"},
-	Run:                   RunList,
+	Use:       "list [ARGUMENT]",
+	Short:     "list [ARGUMENT]",
+	Args:      helpers.ValidateArgs,
+	ValidArgs: []string{"galaxies"},
+	Run:       ExecuteCommand,
+
 	DisableFlagsInUseLine: true,
 }
